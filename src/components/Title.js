22e10ce1390e231108fe6a2e042b9be6e040e1e5
Thilakmani <thilakmani777@gmail.com>
2020-07-30 12:39:27 +0530

    starter files

---
+++ src/components/Title.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Title = () => {
+    return(
+        <div className="title">
+        <h1>ManiGram</h1>
+        <h2>Your Pictures</h2>
+        <p>ManiGram - Thilakmani's gallery | developed by Thilakmani</p>
+        </div>
+    )
+}
+export default Title;
